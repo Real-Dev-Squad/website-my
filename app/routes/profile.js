4bfa5720ce8f82f1dcbea2cb32a8d44a38314679
Swebert Correa <correaswebert@gmail.com>
2021-04-04 02:38:27 +0530

    [WIP] /profile page

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import ENV from 'website-my/config/environment';
 
-export default class ProfileRoute extends Route {
+export default class SignupRoute extends Route {
+  async model() {
+    const response = await fetch(`${ENV.BASE_API_URL}/users/self`, {
+      credentials: 'include',
+    });
+    const userData = await response.json();
+    return userData;
+  }
 }
