f97cfabce0470e8c14058d48ff9602f215777248
Tanya Dixit <shanya.2018td@gmail.com>
2021-04-30 01:17:20 +0530

    update-status feature

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,29 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import ENV from 'website-my/config/environment';
+
+const BASE_URL = ENV.BASE_API_URL;
 
 export default class IndexController extends Controller {
   @tracked status = this.model;
+
+  @action async updateStatus(status) {
+    this.status = status;
+    try {
+      const response = await fetch(`${BASE_URL}/users/self`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          status: status,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+      console.log(response);
+    } catch (error) {
+      console.error('Error : ', error);
+    }
+  }
 }
