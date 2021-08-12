d8721dba0c8789619823aa868be17b23f0823bc5
Swebert Correa <correaswebert@gmail.com>
2021-08-12 20:31:59 +0530

    added functionality to update profile info

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,3 +1,16 @@
 import Route from '@ember/routing/route';
+import ENV from 'website-my/config/environment';
 
-export default class ProfileRoute extends Route {}
+export default class ProfileRoute extends Route {
+  async model() {
+    try {
+      const response = await fetch(`${ENV.BASE_API_URL}/users/self`, {
+        credentials: 'include',
+      });
+      const userData = await response.json();
+      return userData;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+}
