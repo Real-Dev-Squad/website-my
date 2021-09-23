fed0e7fd448770dd4905d1ad2e7af9d72d07ee9b
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2021-09-24 03:32:07 +0530

    Ember Test Selectors Added

--- tests/integration/components/sign-up/get-started-test.js
+++ tests/integration/components/sign-up/get-started-test.js
@@ -9,39 +9,32 @@ module('Integration | Component | sign-up/get-started', function (hooks) {
   test('it renders in get-started state', async function (assert) {
     assert.expect(5);
 
-    this.set('changeRouteParams', function (paramValue) {
-      this.isButtonDisabled = true;
-      if (paramValue)
-        this.transitionToRoute({ queryParams: { state: paramValue } });
-    });
     this.set('state', 'get-started');
 
-    await render(
-      hbs`<SignUp::GetStarted @state={{this.state}} @changeRouteParams={{this.changeRouteParams}} />`
-    );
+    await render(hbs`<SignUp::GetStarted @state={{this.state}} />`);
 
     assert.equal(
-      this.element.querySelector('.landing h2').textContent.trim(),
+      this.element.querySelector('[data-test-mainHeading]').textContent.trim(),
       'Thank you for connecting your GitHub!',
       'Correct Heading'
     );
     assert.equal(
-      this.element.querySelector('.landing h3').textContent.trim(),
+      this.element.querySelector('[data-test-subHeading]').textContent.trim(),
       'Please complete the signup in order to:',
       'Correct Subheading'
     );
     assert.equal(
-      this.element.querySelectorAll('.landing ul li')[0].textContent.trim(),
+      this.element.querySelector('[data-test-li1]').textContent.trim(),
       'Use Features',
       'Correct list item'
     );
     assert.equal(
-      this.element.querySelectorAll('.landing ul li')[1].textContent.trim(),
+      this.element.querySelector('[data-test-li2]').textContent.trim(),
       'Display yourself on the (members) page',
       'Correct list item'
     );
     assert.equal(
-      this.element.querySelector('.get-started__btn button').textContent.trim(),
+      this.element.querySelector('[data-test-button]').textContent.trim(),
       'Get Started',
       'Correct button title'
     );
@@ -50,30 +43,23 @@ module('Integration | Component | sign-up/get-started', function (hooks) {
   test('it renders in thankyou state', async function (assert) {
     assert.expect(3);
 
-    this.set('changeRouteParams', function (paramValue) {
-      this.isButtonDisabled = true;
-      if (paramValue)
-        this.transitionToRoute({ queryParams: { state: paramValue } });
-    });
     this.set('state', 'thank-you');
 
-    await render(
-      hbs`<SignUp::GetStarted @state={{this.state}} @changeRouteParams={{this.changeRouteParams}} />`
-    );
+    await render(hbs`<SignUp::GetStarted @state={{this.state}} />`);
 
     assert.equal(
-      this.element.querySelector('.landing h2').textContent.trim(),
+      this.element.querySelector('[data-test-mainHeading]').textContent.trim(),
       'Congratulations!',
       'Correct Heading'
     );
     assert.equal(
-      this.element.querySelector('.landing h3').textContent.trim(),
+      this.element.querySelector('[data-test-subHeading]').textContent.trim(),
       'Lets get you started on your journey',
       'Correct Subheading'
     );
 
     assert.equal(
-      this.element.querySelector('.get-started__btn button').textContent.trim(),
+      this.element.querySelector('[data-test-button]').textContent.trim(),
       "Let's Go",
       'Correct button title'
     );
