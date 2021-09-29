c69901cb6b4d3fed2d1aa0d1f4aa18fc4c507a36
Rohan Raj Gupta <78433013+rohan09-raj@users.noreply.github.com>
2021-09-29 19:04:30 +0530

    added functionality to update profile info

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,5 +1,7 @@
 import Controller from '@ember/controller';
 import ENV from 'website-my/config/environment';
+import { tracked } from '@glimmer/tracking';
+import { action, set } from '@ember/object';
 const BASE_URL = ENV.BASE_API_URL;
 
 export default class ProfileController extends Controller {
@@ -7,5 +9,280 @@ export default class ProfileController extends Controller {
 
   get imageUploadUrl() {
     return `${BASE_URL}/users/picture`;
+  }
+
+  @tracked isSubmitDisabled = true;
+  @tracked isSubmitClicked = false;
+
+  @tracked title = 'Account Details';
+  @tracked formData = {
+    first_name: '',
+    last_name: '',
+    username: '',
+    email: '',
+    phone: '',
+    yoe: 0,
+    company: '',
+    designation: '',
+    linkedin_id: '',
+    instagram_id: '',
+    twitter_id: '',
+    website: '',
+  };
+
+  formErrors = {
+    first_name: false,
+    last_name: false,
+    username: false,
+    email: false,
+    phone: false,
+    yoe: false,
+    company: false,
+    designation: false,
+    linkedin_id: false,
+    instagram_id: false,
+    twitter_id: false,
+    website: false,
+  };
+
+  @tracked fields = [
+    {
+      id: 'first_name',
+      label: 'First Name',
+      type: 'text',
+      placeholder: 'Darth',
+      errorMessage: 'First name is required',
+      required: true,
+      showError: false,
+      disabled: false,
+    },
+    {
+      id: 'last_name',
+      label: 'Last Name',
+      type: 'text',
+      placeholder: 'Vader',
+      errorMessage: 'Last name is required',
+      required: true,
+      showError: false,
+      disabled: false,
+    },
+    {
+      id: 'username',
+      label: 'Username for Real Dev Squad',
+      type: 'text',
+      placeholder: 'e.g anakin, or some other unique username',
+      errorMessage: 'Username is required',
+      required: true,
+      showError: false,
+      validator: this.userNameValidator,
+      disabled: true,
+    },
+    {
+      id: 'email',
+      label: 'Email',
+      type: 'email',
+      placeholder: 'lukespapa@gmail.com',
+      errorMessage: 'Valid Email is required',
+      required: true,
+      showError: false,
+      validator: this.emailValidator,
+      disabled: false,
+    },
+    {
+      id: 'yoe',
+      label: 'Years of Experience',
+      type: 'number',
+      placeholder: 'How many years have you worked?',
+      errorMessage: 'Number of years of experience is required',
+      required: true,
+      showError: false,
+      disabled: false,
+    },
+    {
+      id: 'company',
+      label: 'Company Name / College Name (Optional)',
+      type: 'text',
+      placeholder: 'Where do you currently work? Death Star? Rebel Base?',
+      errorMessage: '',
+      required: false,
+      showError: false,
+      disabled: false,
+    },
+    {
+      id: 'designation',
+      label: 'Designation (Optional)',
+      placeholder: 'Supreme Commander',
+      type: 'text',
+      errorMessage: '',
+      required: false,
+      showError: false,
+      disabled: false,
+    },
+    {
+      id: 'linkedin_id',
+      label: 'LinkedIn ID (Not the full URL)',
+      placeholder: 'anakin-skywalker-007 i.e just the ID part',
+      type: 'text',
+      errorMessage: 'LinkedIn username is required',
+      required: true,
+      showError: false,
+      disabled: false,
+    },
+    {
+      id: 'instagram_id',
+      label: 'Instagram ID (Optional)',
+      placeholder: 'ForceWielder77',
+      type: 'text',
+      errorMessage: '',
+      required: false,
+      showError: false,
+      disabled: false,
+    },
+    {
+      id: 'twitter_id',
+      label: 'Twitter username (Not the full URL)',
+      placeholder: 'anakin7',
+      type: 'text',
+      errorMessage: 'Twitter handle is required',
+      required: true,
+      showError: false,
+      disabled: false,
+    },
+    {
+      id: 'website',
+      label: 'Website (Optional)',
+      placeholder: 'Your portfolio website if any. e.g mysonisajedi.com',
+      type: 'text',
+      errorMessage: '',
+      required: false,
+      showError: false,
+      disabled: false,
+    },
+  ];
+
+  timerId = undefined;
+
+  //Debounce Function
+  debounce(func, delay) {
+    clearTimeout(this.timerId);
+    this.timerId = setTimeout(func, delay);
+  }
+
+  @action handleFieldChange(name, value) {
+    const index = this.fields.findIndex((field) => field.id === name);
+    set(this.formData, name, value);
+
+    if (this.fields[index].required && !value) {
+      this.isSubmitDisabled = true;
+      set(this.fields[index], 'showError', true);
+    } else if (!this.fields[index].validator) {
+      set(this.fields[index], 'showError', false);
+    }
+
+    const anyErrors = this.fields.map((field) => {
+      return !!(field.required && this.formData[field.id] === '');
+    });
+
+    this.isSubmitDisabled = !anyErrors.filter(Boolean).length;
+  }
+
+  async checkUserName(userName) {
+    if (!userName) {
+      return set(this.fields[2], 'errorMessage', 'Username cannot be empty');
+    }
+    try {
+      const lowerCaseUsername = userName.toLowerCase();
+      const response = await fetch(
+        `${BASE_URL}/users/isUsernameAvailable/${lowerCaseUsername}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        }
+      );
+      const data = await response.json();
+      const { isUsernameAvailable } = data;
+      set(this.fields[2], 'showError', !isUsernameAvailable);
+      set(
+        this.fields[2],
+        'errorMessage',
+        `${userName} is not available, Choose another username!`
+      );
+    } catch (error) {
+      console.error('Error : ', error);
+    }
+  }
+
+  @action async userNameValidator(userName) {
+    this.debounce(() => this.checkUserName(userName), 500);
+  }
+
+  @action phoneNumberValidator(phone) {
+    if (typeof phone !== 'string') {
+      return false;
+    }
+
+    const pattern = /^(0|[+91]{3})?[7-9][0-9]{9}$/;
+    const index = this.fields.findIndex((field) => field.id === 'phone');
+
+    if (pattern.test(phone)) {
+      set(this.fields[index], 'showError', false);
+    } else {
+      set(this.fields[index], 'showError', true);
+    }
+  }
+
+  @action emailValidator(email) {
+    if (typeof email !== 'string') return false;
+
+    const pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const index = this.fields.findIndex((field) => field.type === 'email');
+
+    if (pattern.test(email)) {
+      set(this.fields[index], 'showError', false);
+    } else {
+      set(this.fields[index], 'showError', true);
+    }
+    return;
+  }
+
+  removeEmptyFields(reqObject) {
+    const objectRequested = reqObject;
+    for (const field in objectRequested) {
+      if (!objectRequested[field]) {
+        delete objectRequested[field];
+      }
+    }
+    return objectRequested;
+  }
+
+  @action async handleSubmit(e) {
+    // submit
+    // https://github.com/Real-Dev-Squad/website-api-contracts/tree/main/users#patch-usersself
+    e.preventDefault();
+    const cleanReqObject = this.removeEmptyFields(this.formData);
+    this.isSubmitClicked = true;
+    cleanReqObject.username = cleanReqObject.username.toLowerCase();
+    try {
+      const response = await fetch(`${BASE_URL}/users/self`, {
+        method: 'PATCH',
+        body: JSON.stringify(cleanReqObject),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+
+      const { status } = response;
+      if (status !== 204) {
+        alert('Something went wrong. Please check console errors.');
+      }
+    } catch (error) {
+      console.error('Error : ', error);
+    } finally {
+      this.isSubmitClicked = false;
+    }
   }
 }
--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,3 +1,21 @@
 import Route from '@ember/routing/route';
+import ENV from 'website-my/config/environment';
 
-export default class ProfileRoute extends Route {}
+export default class ProfileRoute extends Route {
+  async model() {
+    try {
+      const response = await fetch(`${ENV.BASE_API_URL}/users/self`, {
+        credentials: 'include',
+      });
+      const userData = await response.json();
+      if (response.status === 401) {
+        alert('You are not logged in. Please login to continue.');
+        window.open('https://my.realdevsquad.com/signup', '_self');
+      } else {
+        return userData;
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+}
