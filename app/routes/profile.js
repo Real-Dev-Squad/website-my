fae394fe52585f5ca97e80c7d5df77aee3362003
Rohan Raj Gupta <78433013+rohan09-raj@users.noreply.github.com>
2021-10-17 16:17:21 +0530

    Fixed the redirection

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -9,13 +9,16 @@ export default class ProfileRoute extends Route {
       });
       const userData = await response.json();
       if (response.status === 401) {
-        alert('You are not logged in. Please login to continue.');
-        window.open('https://my.realdevsquad.com/signup', '_self');
-      } else {
-        return userData;
+        throw new Error('You are not logged in. Please login to continue.');
       }
+      return userData;
     } catch (error) {
       console.error(error.message);
+      alert(error);
+      window.open(
+        'https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97',
+        '_blank'
+      );
     }
   }
 }
