43180b8a873c2c2a2f24b8cf0bad5cde5f9f523c
akshay shinde <akshayshinde7289@gmail.com>
2021-11-02 21:57:37 +0530

    updating cookie

--- app/helpers/setCookie.js
+++ app/helpers/setCookie.js
@@ -3,7 +3,7 @@ import { helper } from '@ember/component/helper';
 function setCookie(mode) {
   const domain = '.realdevsquad.com';
   const expire = new Date(Date.now() + 24 * 31 * 60 * 60 * 1000);
-  const cookie = `mode=${mode}; domain=${domain}; expires=${expire} path=/`;
+  const cookie = `theme=${mode}; domain=${domain}; expires=${expire} path=/`;
   document.cookie = cookie;
 
   if (mode == 'light') {
