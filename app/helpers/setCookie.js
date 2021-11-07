85e47b46976411321b3d93e3015b1201893f1af2
akshay shinde <akshayshinde7289@gmail.com>
2021-11-07 20:00:03 +0530

    using urls for image src

--- app/helpers/setCookie.js
+++ app/helpers/setCookie.js
@@ -2,11 +2,11 @@ import { helper } from '@ember/component/helper';
 
 function setCookie(mode) {
   const domain = '.realdevsquad.com';
-  const expire = new Date(Date.now() + 24 * 31 * 60 * 60 * 1000);
-  const cookie = `theme=${mode}; domain=${domain}; expires=${expire} path=/`;
+  const cookieExpiryDate = new Date(Date.now() + 24 * 31 * 60 * 60 * 1000);
+  const cookie = `theme=${mode}; domain=${domain}; expires=${cookieExpiryDate} path=/`;
   document.cookie = cookie;
 
-  if (mode == 'light') {
+  if (mode === 'light') {
     document.body.classList.remove('darkTheme');
     document.body.classList.add('lightTheme');
   } else {
