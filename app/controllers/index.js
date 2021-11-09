387277fe4a337a006cc0a60372a399dba89dd2a2
Rohan Raj Gupta <78433013+rohan09-raj@users.noreply.github.com>
2021-11-10 01:43:21 +0530

    updating user status  feature

---
+++ app/controllers/index.js
@@ -0,0 +1,32 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import ENV from 'website-my/config/environment';
+
+const BASE_URL = ENV.BASE_API_URL;
+
+export default class IndexController extends Controller {
+  @tracked status = this.model;
+
+  @action async updateStatus(status) {
+    this.status = status;
+    try {
+      const response = await fetch(`${BASE_URL}/users/self`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          status,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+      const { status } = response;
+      if (status !== 204) {
+        alert('Something went wrong. Please check console errors.');
+      }
+    } catch (error) {
+      console.error('Error : ', error);
+    }
+  }
+}
