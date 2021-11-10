6d0011ccca1315e086c5c3e6e4bcafaae4c4a487
akshay shinde <akshayshinde7289@gmail.com>
2021-11-10 20:37:12 +0530

    importing dark and light const

--- app/components/local-navbar.js
+++ app/components/local-navbar.js
@@ -1,17 +1,17 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import setCookie from '../helpers/setCookie';
-import { SUN_URL, MOON_URL } from '../constants/url';
+import { SUN_URL, MOON_URL, THEME } from '../constants/theme';
 
 export default class ExampleComponent extends Component {
   @action
   changeTheme(event) {
     if (event.target.src.includes(MOON_URL)) {
       event.target.src = SUN_URL;
-      setCookie.compute('dark');
+      setCookie.compute(THEME.DARK);
     } else {
       event.target.src = MOON_URL;
-      setCookie.compute('light');
+      setCookie.compute(THEME.LIGHT);
     }
   }
 }
--- app/constants/theme.js
+++ app/constants/theme.js
@@ -1,2 +1,6 @@
 export const SUN_URL = 'assets/sun.png';
 export const MOON_URL = 'assets/moon.png';
+export const THEME = {
+  LIGHT: 'light',
+  DARK: 'dark',
+};
