94bd199551ca61c25273ceda891bdb3e80d5882b
Ankush Dharkar <ankushdharkar@users.noreply.github.com>
2021-11-12 08:49:43 -0800

    Added test to ensure return type from FormInput component #129 (#130)

--- tests/integration/components/form-input-test.js
+++ tests/integration/components/form-input-test.js
@@ -2,25 +2,89 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import fillIn from '@ember/test-helpers/dom/fill-in';
 
 module('Integration | Component | form-input', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<FormInput />`);
-
-    assert.equal(this.element.textContent.trim(), ':');
-
-    // Template block usage:
+    // Assemble
     await render(hbs`
-      <FormInput>
-        template block text
-      </FormInput>
+      <FormInput />
     `);
 
-    assert.equal(this.element.textContent.trim(), ':');
+    // Assert
+    assert.dom('input').exists();
+  });
+
+  test('it shows the error', async function (assert) {
+    // Assemble
+    await render(hbs`
+      <FormInput
+        @errorMessage={{'SAMPLE ERROR'}}
+        @required={{true}}
+        @showError={{true}}
+      />
+    `);
+
+    // Assert
+    assert.dom('.error').exists();
+  });
+
+  test('it returns the right type of input entered for number', async function (assert) {
+    // Assemble
+    const NUMBER_TYPE = 'number';
+    const NUMBER_INPUT = 12345;
+
+    // Assemble
+    this.set('onChange', (inputValue) => {
+      this.set('inputValue', inputValue);
+
+      if (typeof inputValue !== NUMBER_TYPE) {
+        assert.step('Wrong Type');
+      }
+    });
+
+    await render(hbs`
+      <FormInput
+        @type={{NUMBER_TYPE}}
+        @value={{this.inputValue}}
+        @onChange={{this.onChange}}
+      />
+    `);
+
+    // Act
+    await fillIn('input', NUMBER_INPUT);
+
+    // Assert
+    assert.verifySteps([]);
+  });
+
+  test('it returns the right type of input entered for string', async function (assert) {
+    const STRING_TYPE = 'string';
+    const STRING_INPUT = '12345';
+
+    // Assemble
+    this.set('onChange', (inputValue) => {
+      this.set('inputValue', inputValue);
+
+      if (typeof inputValue !== STRING_TYPE) {
+        assert.step('Wrong Type');
+      }
+    });
+
+    await render(hbs`
+      <FormInput
+        @type={{STRING_TYPE}}
+        @value={{this.inputValue}}
+        @onChange={{this.onChange}}
+      />
+    `);
+
+    // Act
+    await fillIn('input', STRING_INPUT);
+
+    // Assert
+    assert.verifySteps([]);
   });
 });
