62c80d5408f3f645098c2e9b9f6e887b4a26d388
akshay shinde <akshayshinde7289@gmail.com>
2021-11-17 01:32:53 +0530

    refactoring setCookie

--- app/components/local-navbar.js
+++ app/components/local-navbar.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import setTheme from '../helpers/setTheme';
 import setCookie from '../helpers/setCookie';
 import { SUN_URL, MOON_URL, THEME } from '../constants/theme';
 
@@ -8,9 +9,11 @@ export default class ExampleComponent extends Component {
   changeTheme(event) {
     if (event.target.src.includes(MOON_URL)) {
       event.target.src = SUN_URL;
+      setTheme.compute(THEME.DARK);
       setCookie.compute(THEME.DARK);
     } else {
       event.target.src = MOON_URL;
+      setTheme.compute(THEME.LIGHT);
       setCookie.compute(THEME.LIGHT);
     }
   }
