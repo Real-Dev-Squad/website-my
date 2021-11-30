fe5e06340db1d47856c31d8fccc6dc3305bdbb65
Swaraj <swarajrajpure@gmail.com>
2021-12-01 00:17:12 +0530

    chore: move from Input component to native html input and add styles

--- app/components/input-field.js
+++ app/components/input-field.js
@@ -1,33 +1,31 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
 export default class InputFieldComponent extends Component {
-  @tracked inputValue = null;
-
   @action handleInputFieldChange(event) {
+    let inputValue;
     const { type, onChange, validator } = this.args;
     const { value } = event.target;
 
     switch (type) {
       case 'string':
-        this.inputValue = value;
+        inputValue = value;
         break;
       case 'number':
-        this.inputValue = Number(value);
+        inputValue = Number(value);
         break;
       case 'boolean':
-        this.inputValue = Boolean(value);
+        inputValue = Boolean(value);
         break;
       default:
-        this.inputValue = value;
+        inputValue = value;
         console.warn('Unknown type detected! Keeping the value as string');
     }
 
     if (typeof validator === 'function') {
-      validator(this.inputValue);
+      validator(inputValue);
     }
 
-    onChange(this.inputValue);
+    onChange(inputValue);
   }
 }
