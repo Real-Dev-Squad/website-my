3cc578ede9e09c26d2b297c40e3f82b9214a110d
Rohan Raj Gupta <rajrohan1914@gmail.com>
2021-12-18 21:48:42 +0530

    Handled edge cases for user status

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,13 @@ import Route from '@ember/routing/route';
 import ENV from 'website-my/config/environment';
 
 const API_BASE_URL = ENV.BASE_API_URL;
-
 export default class IndexRoute extends Route {
   model = async () => {
+    const defaultStatus = 'active';
     const response = await fetch(`${API_BASE_URL}/users/self`, {
       credentials: 'include',
     });
     const userData = await response.json();
-    return userData.status;
+    return userData.status ?? defaultStatus;
   };
 }
