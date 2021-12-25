4611362d061675e1a262ef21c11d759261815d80
Rohan Raj Gupta <rajrohan1914@gmail.com>
2021-12-26 01:00:50 +0530

    Added disabled attribute to user-status buttons

--- app/components/user-status.js
+++ app/components/user-status.js
@@ -1,5 +1,4 @@
 import Component from '@glimmer/component';
-
 export default class UserStatusComponent extends Component {
   currentUserStatus = [
     {
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,19 +5,23 @@ const API_BASE_URL = ENV.BASE_API_URL;
 export default class IndexRoute extends Route {
   model = async () => {
     const defaultStatus = 'active';
-    const response = await fetch(`${API_BASE_URL}/users/self`, {
-      credentials: 'include',
-    });
-    const userData = await response.json();
-    if (response.status === 401) {
-      alert('You are not logged in. Please login to continue.');
-      window.open(
-        'https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97',
-        '_self'
-      );
-    }
-    if (response.status === 200 && !userData.incompleteUserDetails) {
-      return userData.status ?? defaultStatus;
+    try {
+      const response = await fetch(`${API_BASE_URL}/users/self`, {
+        credentials: 'include',
+      });
+      const userData = await response.json();
+      if (response.status === 200 && !userData.incompleteUserDetails) {
+        return userData.status ?? defaultStatus;
+      } else if (response.status === 401) {
+        alert('You are not logged in. Please login to continue.');
+        window.open(
+          'https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97',
+          '_self'
+        );
+      }
+    } catch (error) {
+      console.error(error.message);
+      alert(`Failed`);
     }
   };
 }
