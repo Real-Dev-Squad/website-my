d9641f45d37f1e9bae28d23cdae1106ca51f5835
akshay shinde <akshayshinde7289@gmail.com>
2021-12-27 20:00:29 +0530

    css changes

--- app/helpers/setCookie.js
+++ app/helpers/setCookie.js
@@ -2,7 +2,7 @@ import { helper } from '@ember/component/helper';
 
 function setCookie(theme) {
   const domain = '.realdevsquad.com';
-  const cookieExpiryDate = new Date(Date.now() + 24 * 31 * 60 * 60 * 1000);
+  const cookieExpiryDate = new Date(Date.now() + 31 * 24 * 60 * 60 * 1000);
   const cookie = `theme=${theme}; domain=${domain}; expires=${cookieExpiryDate} path=/`;
   document.cookie = cookie;
 }
