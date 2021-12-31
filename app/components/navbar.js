a8e66c223e8f1e1b7b7362ea3e13811d5a9ca49c
user.name: "Rohan Raj Gupta <user.email: "rajrohan1914@gmail.com>
2021-12-31 19:04:48 +0530

    fixed formatting in components files

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -3,6 +3,9 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class NavbarComponent extends Component {
+  @tracked isNavOpen = true;
+
+  // Linked Site URLs
   HOME_PAGE_URL = 'http://realdevsquad.com/';
   WELCOME_SITE_URL = 'https://welcome.realdevsquad.com/';
   EVENTS_SITE_URL = 'http://realdevsquad.com/events.html';
@@ -10,9 +13,7 @@ export default class NavbarComponent extends Component {
   CRYPTO_SITE_URL = 'https://crypto.realdevsquad.com/';
   STATUS_SITE_URL = 'https://status.realdevsquad.com/';
 
-  @tracked isOpen = true;
-
   @action toggleNav() {
-    this.isOpen = !this.isOpen;
+    this.isNavOpen = !this.isNavOpen;
   }
 }
