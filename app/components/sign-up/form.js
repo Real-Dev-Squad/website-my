1f531f729da111c9beec82994ce3b81466a549ce
rohan09-raj <rajrohan1914@gmail.com>
2022-02-06 16:37:54 +0530

    fixed changes

--- app/components/sign-up/form.js
+++ app/components/sign-up/form.js
@@ -1,9 +1,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { LABEL_TEXT } from '../../constants/signup';
-import mixpanel from 'mixpanel-browser';
-
-mixpanel.init('5fcfb02eabfc77f5a6a0e4cb65bbf5e0', { debug: true });
 export default class SignupComponent extends Component {
   get label() {
     const { state } = this.args;
@@ -12,7 +9,6 @@ export default class SignupComponent extends Component {
   }
 
   @action inputFieldChanged({ target: { value } }) {
-    mixpanel.track('New Signup Flow in Progress');
     const { onChange, state } = this.args;
     onChange(state, value);
   }
