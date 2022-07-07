1b949c1839238193733fec4f5400fd6fdbcc61a3
rohan09-raj <rajrohan1914@gmail.com>
2022-07-07 13:22:00 +0530

    fixed naming in tests

--- tests/unit/utils/mixpanel-analytics-test.js
+++ tests/unit/utils/mixpanel-analytics-test.js
@@ -3,8 +3,8 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | mixpanel-analytics', function () {
   test('it returns the event passed in trackEvent util', function (assert) {
-    let result = 'Test Testing';
-    let input = mixpanelAnalytics().trackEvent('Test Testing').event;
-    assert.equal(input, result);
+    let eventNameResult = 'Test Testing';
+    let eventName = mixpanelAnalytics().trackEvent('Test Testing').event;
+    assert.equal(eventNameResult, eventName);
   });
 });
