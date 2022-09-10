dcdc30ab64d10fbf9fda71a6c25d689526d60212
shreya-mishra <shreyamishra040398@gmail.com>
2022-09-10 23:42:54 +0530

    resolved signup flow test issue

---
+++ tests/acceptance/signup-flow-test.js
@@ -0,0 +1,78 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, fillIn, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import {fields} from '../mocks/user-data';
+import { setupWorker } from 'msw';
+import { handlers } from '../mocks/handler';
+import sinon from 'sinon';
+
+module('Acceptance | signup flow', function (hooks) {
+  const worker = setupWorker(...handlers);
+
+  hooks.beforeEach(() => {
+    worker.start();
+   
+  });
+  hooks.afterEach(() => {
+    worker.stop();
+  });
+	
+
+  setupApplicationTest(hooks);
+
+  test('signup flow', async function (assert) {
+        
+    await visit('/signup');
+    assert.equal(currentURL(), '/signup');
+
+    assert.dom('[data-test-id="signup-button-disabled"]').exists();
+    assert.dom('[data-test-id="signup-button"]').doesNotExist();
+
+    for (let field in fields) {
+      await fillIn(`[data-test-id="form-input-${field}"]`, fields[field]);
+    }
+
+
+    assert
+      .dom('[data-test-id="form-input-first_name"]')
+      .hasValue('test');
+    assert
+      .dom('[data-test-id="form-input-last_name"]')
+      .hasValue('user');
+    assert
+      .dom('[data-test-id="form-input-username"]')
+      .hasValue('test-user');
+    assert
+      .dom('[data-test-id="form-input-email"]')
+      .hasValue('test@user.com');
+    assert
+      .dom('[data-test-id="form-input-yoe"]')
+      .hasValue('0');
+    assert
+      .dom('[data-test-id="form-input-company"]')
+      .hasValue('rds');
+    assert
+      .dom('[data-test-id="form-input-designation"]')
+      .hasValue('demo');
+    assert
+      .dom('[data-test-id="form-input-linkedin_id"]')
+      .hasValue('demo@linkedin');
+    assert
+      .dom('[data-test-id="form-input-instagram_id"]')
+      .hasValue('demo@insta');
+    assert
+      .dom('[data-test-id="form-input-twitter_id"]')
+      .hasValue('demo@twitter');
+    assert
+      .dom('[data-test-id="form-input-website"]')
+      .hasValue('test.com');
+
+    assert.dom('[data-test-id="signup-button-disabled"]').doesNotExist();
+    assert.dom('[data-test-id="signup-button"]').exists();
+
+
+    await click('[data-test-id="signup-button"]')
+ 
+
+  });
+});
---
+++ tests/mocks/user-data.js
@@ -0,0 +1,13 @@
+export const fields = {
+  first_name:"test",
+  last_name:'user',
+  username:'test-user',
+  email:'test@user.com',
+  yoe:'0',
+  company:'rds',
+  designation:'demo',
+  linkedin_id:'demo@linkedin',
+  instagram_id:'demo@insta',
+  twitter_id:'demo@twitter',
+  website:'test.com'
+}
