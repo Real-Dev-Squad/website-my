69c2dae8d691a5174653440be8b9923585f281f0
Abhay <abhay@sourcewiz.co>
2022-09-26 22:52:28 +0530

    feat places suggestion added in the modal

--- app/components/modal-location.js
+++ app/components/modal-location.js
@@ -1,99 +1,116 @@
 /* eslint-disable prettier/prettier */
-
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
 
 import ENV from 'website-my/config/environment';
 
 const BASE_URL = ENV.GEOCODING_API_URL;
 
+
 export default class ModalLocationComponent extends Component {
-  @tracked isOpen = false;
 
-  @tracked location = '';
+    @tracked isOpen = false;
 
-  @tracked updateLocation = null;
-
-  @tracked sendNotification = null;
-
-  @tracked isCheckAll = false;
-
-  @tracked isLoading = false;
-
-  @tracked placesArray = [];
-
-  @action setSelection(event) {
-    this.updateLocation = event.target.value;
-
-    console.log(this.updateLocation, 'update location option');
-  }
-
-  @action setSelectedNotification(event) {
-    this.sendNotification = event.target.value;
-
-    console.log(this.sendNotification, 'notification sent to');
-  }
-
-  @action async updateValue(event) {
-    this.location = event.target.value;
-
-    this.isLoading = true;
-
-    try {
-      const response = await fetch(
-        `${BASE_URL}/${this.location}.json?proximity=ip&types=place%2Cpostcode%2Caddress&access_token=pk.eyJ1IjoidmluYXlhay0wOSIsImEiOiJjbDdoM24waG0wYW9tM3dwYjN3aWJpNGh4In0.pcwljIKYp_NL-GmtDztvPg`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (response.ok) {
-        this.isLoading = false;
-        console.log(response, 'result url');
-
-        const result = await response.json();
-
-        this.placesArray = result?.features;
-
-        console.log(this.placesArray, '____result');
-      }
-    } catch (error) {
-      this.isLoading = false;
-      console.log(error.message, '__error');
-    } finally {
-      this.isLoading = false;
+    @tracked location = '';
+  
+    @tracked updateLocation = null;
+  
+    @tracked sendNotification = null;
+  
+    @tracked isCheckAll = false;
+  
+    @tracked isLoading = false;
+  
+    @tracked placesArray = [];
+  
+    @action setSelection(event) {
+      this.updateLocation = event.target.value;
+  
+      console.log(this.updateLocation, 'update location option');
     }
-  }
+  
+    @action setSelectedNotification(event) {
+      this.sendNotification = event.target.value;
+  
+      console.log(this.sendNotification, 'notification sent to');
+    }
+  
+  
+    @action handleSelectedPlace(places) {
+  
+          console.log('clicked' , places);
+          
 
-  @action toggleModal() {
-    this.isOpen = !this.isOpen;
-  }
+          this.location = places;
+    }
+  
+    @action async updateValue(event) {
+      this.location = event.target.value;
+  
+      this.isLoading = true;
+  
+      try {
+        const response = await fetch(
+          `${BASE_URL}/${this.location}.json?limit=4&proximity=ip&types=place%2Cpostcode%2Caddress&access_token=pk.eyJ1IjoidmluYXlhay0wOSIsImEiOiJjbDdoM24waG0wYW9tM3dwYjN3aWJpNGh4In0.pcwljIKYp_NL-GmtDztvPg`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+  
+        if (response.ok) {
+          this.isLoading = false;
+          console.log(response, 'result url');
+  
+          const result = await response.json();
+  
+          this.placesArray = result?.features;
+  
+          console.log(this.placesArray, '____result');
+        }
+      } catch (error) {
+        this.isLoading = false;
+        console.log(error.message, '__error');
+      } finally {
+        this.isLoading = false;
+      }
+    }
+  
+    @action toggleModal() {
+      this.isOpen = !this.isOpen;
+    }
+  
+    formDataKeyName = 'modal';
+  
+    @tracked isSubmitDisabled = false;
+  
+    @action async handleSubmit(e) {
+      e.preventDefault();
+  
+      this.isOpen = false;
+  
+      const cleanReqObject = {
+        location: this.location,
+  
+        updateLocation: this.updateLocation,
+  
+        sendNotification: this.sendNotification,
+      };
+  
+      this.isSubmitClicked = true;
+  
+      this.location = '';
+  
+      console.log(cleanReqObject, 'feilds value');
+    }
 
-  formDataKeyName = 'modal';
+    
 
-  @tracked isSubmitDisabled = false;
+     
+   
 
-  @action async handleSubmit(e) {
-    e.preventDefault();
-
-    this.isOpen = false;
-
-    const cleanReqObject = {
-      location: this.location,
-
-      updateLocation: this.updateLocation,
-
-      sendNotification: this.sendNotification,
-    };
-
-    this.isSubmitClicked = true;
-
-    this.location = '';
-
-    console.log(cleanReqObject, 'feilds value');
-  }
 }
