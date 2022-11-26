f0bea79a1e43aa732ce38f0033edc245839f09aa
Vinit khandal <vinit224488@gmail.com>
2022-11-26 13:38:26 +0530

    add notification feature-flag

--- app/routes/notifications.js
+++ app/routes/notifications.js
@@ -1,7 +1,19 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class NotificationsRoute extends Route {
-  async model() {
+  @service router;
+  queryParams = {
+    dev: {
+      refreshModel: false,
+    },
+  };
+  beforeModel(transition) {
+    if (transition?.to?.queryParams?.dev !== 'true') {
+      this.router.transitionTo('');
+    }
+  }
+  async model(params) {
     let response = await fetch('/api/notification.json');
     let parsed = await response.json();
     return parsed.data;
