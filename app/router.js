71845e5293fe3017270abcb1fd2450ea3d007079
Vinit khandal <vinit224488@gmail.com>
2022-11-27 12:42:44 +0530

    add line space and remove serice route

--- app/router.js
+++ app/router.js
@@ -1,16 +1,16 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'website-my/config/environment';
-import { inject as service } from '@ember/service';
 export default class Router extends EmberRouter {
-  @service router;
   location = config.locationType;
   rootURL = config.rootURL;
+
   queryParams = {
     dev: {
       refreshModel: false,
     },
   };
 }
+
 Router.map(function () {
   this.route('signup');
   this.route('new-signup');
--- app/routes/notifications.js
+++ app/routes/notifications.js
@@ -3,16 +3,19 @@ import { inject as service } from '@ember/service';
 
 export default class NotificationsRoute extends Route {
   @service router;
+
   queryParams = {
     dev: {
       refreshModel: false,
     },
   };
+
   beforeModel(transition) {
     if (transition?.to?.queryParams?.dev !== 'true') {
       this.router.transitionTo('');
     }
   }
+
   async model(params) {
     let response = await fetch('/api/notification.json');
     let parsed = await response.json();
