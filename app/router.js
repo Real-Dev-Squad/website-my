70e3d274a0c2e7adb9816d1764279a2c5a83dd18
Vinit khandal <vinit224488@gmail.com>
2022-11-27 14:57:19 +0530

    refractor router js

--- app/router.js
+++ app/router.js
@@ -1,14 +1,9 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'website-my/config/environment';
+
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
-
-  // queryParams = {
-  //   dev: {
-  //     refreshModel: false,
-  //   },
-  // };
 }
 
 Router.map(function () {
