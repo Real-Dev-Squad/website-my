c357d210d09765dd3f51a6eca414b6bf577d37e5
Vinit khandal <111434418+vinit717@users.noreply.github.com>
2022-11-27 15:08:35 +0530

    add notification feature-flag (#323)
    * add notification feature-flag
    
    * remove extra space in navbar
    
    * add line space and remove serice route
    
    * remove params
    
    * refractor router js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,10 +1,12 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class NavbarComponent extends Component {
   @tracked isNavOpen = false;
   @tracked isMenuOpen = false;
+  @service router;
 
   HOME_PAGE_URL = 'http://realdevsquad.com/';
 
@@ -41,5 +43,9 @@ export default class NavbarComponent extends Component {
 
   @action outsideClickMenu() {
     this.isMenuOpen = false;
+  }
+
+  get isDev() {
+    return this.router.currentRoute.queryParams.dev;
   }
 }
