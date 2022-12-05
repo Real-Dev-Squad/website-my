a5185cb00c18f56ebc4f4b463adf6cbf385c226d
Abhay <ap3939529@gmail.com>
2022-12-05 14:10:50 +0530

    feat:add tests for the modal component

--- tests/integration/components/modal-location-test.js
+++ tests/integration/components/modal-location-test.js
@@ -2,22 +2,71 @@
 /* eslint-disable prettier/prettier */
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render} from '@ember/test-helpers';
+import { click, render, fillIn } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | modal-location', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    
     await render(hbs`<ModalLocation />`);
-
-   
 
     assert.dom('.update__link').exists();
     assert.dom('.update__link').hasText('update location');
+  });
 
+  test('should open modal', async function (assert) {
+    assert.expect(2);
 
-   
+    await render(hbs`<ModalLocation/>`);
+
+    assert.dom('[data-test-update-link]').exists();
+    assert.dom('[data-test-update-link]').hasText('update location');
+
+    // Click on the button
+    await click('.update__link');
+  });
+
+  test('modal opened', async function (assert) {
+    await render(hbs`<ModalLocation/>`);
+
+    await click('.update__link');
+
+    const STRING_TYPE = 'string';
+    const STRING_INPUT = 'Pune';
+
+    this.set('STRING_TYPE', STRING_TYPE);
+
+    this.set('onChange', (inputValue) => {
+      this.set('inputValue', inputValue);
+    });
+
+    // Act
+
+    await fillIn('[data-test-input-location]', STRING_INPUT);
+
+    await fillIn('[data-test-update_location]', ['travelling'], true);
+
+    await fillIn('[data-test-send-notification]', ['Developers'], true);
+
+    // await this.pauseTest();
+
+    assert.dom('[data-test-submit-btn]').exists();
+
+    assert.dom('[data-test-submit-btn]').hasAttribute('type', 'submit');
+
+    await click('[data-test-submit-btn]');
+
+    assert.verifySteps([]);
+  });
+
+  test('cross mark icon closes icon', async function (assert) {
+    await render(hbs`<ModalLocation/>`);
+
+    await click('.update__link');
+
+    assert.dom('[data-test-cross-icon]').exists();
+
+    await click('[data-test-cross-icon]');
   });
 });
