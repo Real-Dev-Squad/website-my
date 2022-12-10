8b370d2a6c48995b22931337ef1d28ac9519db81
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2022-12-10 13:04:26 +0530

    handles the case when user status not found in collection (#337)
    * handles the case when user status not found in collection
    
    * Resolves Rohan Raj comments on #337

--- app/components/user-status.js
+++ app/components/user-status.js
@@ -1,36 +1,56 @@
 import Component from '@glimmer/component';
 import { USER_STATES } from '../constants/user-status';
 export default class UserStatusComponent extends Component {
+  ALL_FEASIBLE_STATUS = {
+    [USER_STATES.ACTIVE]: {
+      status: USER_STATES.ACTIVE,
+      message: 'Change your status to Active',
+      class: 'buttons__active',
+    },
+    [USER_STATES.IDLE]: {
+      status: USER_STATES.IDLE,
+      message: 'Change your status to Idle',
+      class: 'buttons__idle',
+    },
+    [USER_STATES.OOO]: {
+      status: USER_STATES.OOO,
+      message: 'Change your status to OOO',
+      class: 'buttons__ooo',
+    },
+  };
   currentUserStatus = [
     {
       status: USER_STATES.ACTIVE,
       message: 'You are Active',
-      firstAvailableStatus: USER_STATES.IDLE,
-      firstStatusMessage: 'Change your status to Idle',
-      firstStatusClass: 'buttons__idle',
-      secondAvailableStatus: USER_STATES.OOO,
-      secondStatusMessage: 'Change your status to OOO',
-      secondStatusClass: 'buttons__ooo',
+      otherAvailableStatus: [
+        this.ALL_FEASIBLE_STATUS.IDLE,
+        this.ALL_FEASIBLE_STATUS.OOO,
+      ],
     },
     {
       status: USER_STATES.IDLE,
       message: 'You are Idle',
-      firstAvailableStatus: USER_STATES.ACTIVE,
-      firstStatusMessage: 'Change your status status to Active',
-      firstStatusClass: 'buttons__active',
-      secondAvailableStatus: USER_STATES.OOO,
-      secondStatusMessage: 'Change your status status to OOO',
-      secondStatusClass: 'buttons__ooo',
+      otherAvailableStatus: [
+        this.ALL_FEASIBLE_STATUS.ACTIVE,
+        this.ALL_FEASIBLE_STATUS.OOO,
+      ],
     },
     {
       status: USER_STATES.OOO,
       message: 'You are OOO',
-      firstAvailableStatus: USER_STATES.IDLE,
-      firstStatusMessage: 'Change your status status to Idle',
-      firstStatusClass: 'buttons__idle',
-      secondAvailableStatus: USER_STATES.ACTIVE,
-      secondStatusMessage: 'Change your status status to Active',
-      secondStatusClass: 'buttons__active',
+      otherAvailableStatus: [
+        this.ALL_FEASIBLE_STATUS.ACTIVE,
+        this.ALL_FEASIBLE_STATUS.IDLE,
+      ],
+    },
+    {
+      status: USER_STATES.DNE,
+      message: `Your Status doesn't exist`,
+      otherAvailableStatus: [
+        this.ALL_FEASIBLE_STATUS.ACTIVE,
+        this.ALL_FEASIBLE_STATUS.IDLE,
+        this.ALL_FEASIBLE_STATUS.OOO,
+      ],
     },
   ];
 }
--- app/constants/user-status.js
+++ app/constants/user-status.js
@@ -4,6 +4,11 @@ export const WARNING_MESSAGE_FOR_OOO =
   'The Reason Field is mandatory. Please mention the reason for going OOO.';
 export const WARNING_MESSAGE_FOR_IDLE =
   'The Missing Skills Field is mandatory. Please mention the skills you are lagging in.';
-export const USER_STATES = { IDLE: 'IDLE', ACTIVE: 'ACTIVE', OOO: 'OOO' };
+export const USER_STATES = {
+  IDLE: 'IDLE',
+  ACTIVE: 'ACTIVE',
+  OOO: 'OOO',
+  DNE: 'DNE',
+};
 export const WARNING_FROM_DATE_EXCEEDS_UNTIL_DATE =
   'Until date cant lie before the From date. Please recheck the dates again.';
