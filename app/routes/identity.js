88c426c01284edb93e97e8b08f58e43266849af1
ivinayakg <vinayak20029@gmail.com>
2022-12-27 13:00:01 +0530

    added new signup flow to all pages

--- app/routes/identity.js
+++ app/routes/identity.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import ENV from 'website-my/config/environment';
 import { inject as service } from '@ember/service';
 import { toastNotificationTimeoutOptions } from '../constants/toast-notification';
+import { AUTH_URL } from '../constants/signup';
 
 export default class IdentityRoute extends Route {
   @service toast;
@@ -19,14 +20,13 @@ export default class IdentityRoute extends Route {
       console.error(error.message);
       this.toast.error(error, '', toastNotificationTimeoutOptions);
       // added setTimeout here because before new page opens user should be notified of error by toast
-      setTimeout(
-        () =>
-          window.open(
-            'https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97',
-            '_blank'
-          ),
-        2000
-      );
+      setTimeout(() => {
+        let authUrl = AUTH_URL;
+        if (typeof window !== 'undefined') {
+          authUrl = `${authUrl}&state=${window.location.href}`;
+        }
+        window.open(authUrl, '_self');
+      }, 2000);
     }
   }
 }
--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import ENV from 'website-my/config/environment';
 import { inject as service } from '@ember/service';
 import { toastNotificationTimeoutOptions } from '../constants/toast-notification';
+import { AUTH_URL } from '../constants/signup';
 
 const API_BASE_URL = ENV.BASE_API_URL;
 
@@ -22,14 +23,13 @@ export default class TasksRoute extends Route {
     } catch (error) {
       this.toast.error(error, '', toastNotificationTimeoutOptions);
       // added setTimeout here because before new page opens user should be notified of error by toast
-      setTimeout(
-        () =>
-          window.open(
-            'https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97',
-            '_self'
-          ),
-        2000
-      );
+      setTimeout(() => {
+        let authUrl = AUTH_URL;
+        if (typeof window !== 'undefined') {
+          authUrl = `${authUrl}&state=${window.location.href}`;
+        }
+        window.open(authUrl, '_self');
+      }, 2000);
     }
   };
 }
