8905bff099f7247485e97838746b091391872ffd
Sreejith K <srejitk@gmail.com>
2023-01-31 00:10:59 +0530

    Feat : Added Constants

--- app/constants/url.js
+++ app/constants/url.js
@@ -1,1 +1,9 @@
 export const GITHUB_URL = 'https://github.com/Real-Dev-Squad/website-my';
+export const RDS_MAIN_URL = 'http://realdevsquad.com/';
+export const RDS_WELCOME_URL = 'https://welcome.realdevsquad.com/';
+export const RDS_EVENTS_URL = 'http://realdevsquad.com/events.html';
+export const RDS_MEMBERS_URL = 'https://members.realdevsquad.com/';
+export const RDS_CRYPTO_URL = 'https://crypto.realdevsquad.com/';
+export const RDS_STATUS_URL = 'https://status.realdevsquad.com/';
+export const LOGOUT_URL = 'https://api.realdevsquad.com/auth/signout';
+export const MAIN_SITE_URL = 'https://realdevsquad.com/';
