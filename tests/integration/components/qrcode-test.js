02599b23d1bfd74490cebb16759c87ca3dedb72e
shreya-mishra <shreyamishra040398@gmail.com>
2023-04-04 00:29:59 +0530

    added test

---
+++ tests/integration/components/qrcode-test.js
@@ -0,0 +1,20 @@
+import { render } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | qr-code', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders a QR code with the correct text and dark color', async function(assert) {
+    this.set('userId', '12345');
+
+    await render(hbs`
+          <Qrcode
+           @userId={{this.userId}}
+          />    
+    `);
+
+    assert.dom('[data-test="qr-code"]').hasTagName('svg')
+  });
+});
