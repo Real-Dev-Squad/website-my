1a08d0eec61ac4f00efc1ae3b476b20864709546
shreya-mishra <shreyamishra040398@gmail.com>
2023-04-28 05:59:16 +0530

    fix lint issue

--- app/controllers/mobile.js
+++ app/controllers/mobile.js
@@ -1,12 +1,3 @@
 import MobileRoute from '../routes/mobile';
 
-export default class MobileController extends MobileRoute {
-    @service router;
-
-    get isDev() {
-        if (this.router.currentRoute) {
-          return this.router.currentRoute.queryParams.dev;
-        }
-        return false;
-      }
-}
+export default class MobileController extends MobileRoute {}
