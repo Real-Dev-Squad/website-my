45baa754fa3b2df3c8bd5f2b2939db5ad180c605
shreya-mishra <shreyamishra040398@gmail.com>
2023-05-11 03:00:31 +0530

    added confirmation to verify the user

--- app/controllers/mobile.js
+++ app/controllers/mobile.js
@@ -1,3 +1,12 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
-export default class MobileController extends Controller {}
+export default class MobileController extends Controller {
+  @action buttonClicked() {
+    if (confirm('Are you sure you are the one who scanned this QR code?')) {
+      // will do api call to authenticate the user
+    } else {
+      // will cancel the login
+    }
+  }
+}
