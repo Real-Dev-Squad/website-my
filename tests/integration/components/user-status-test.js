0b13e12b3a004dff3f7c04d20c090ea93693b7c2
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-05-25 03:00:50 +0530

    added test for when button is clicked

--- tests/integration/components/user-status-test.js
+++ tests/integration/components/user-status-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | user-status', function (hooks) {
@@ -10,11 +10,27 @@ module('Integration | Component | user-status', function (hooks) {
     this.setProperties({
       status: 'DNE',
       isStatusUpdating: false,
+      updateStatus: (statusPayLoad) => {
+        const {
+          currentStatus: { updatedAt, from, state  },
+        } = statusPayLoad;
+        assert.equal(state, 'ACTIVE', 'new state present in the payload');
+        assert.ok(typeof from === 'number', 'from is a numeric timestamp');
+        assert.ok(
+          typeof updatedAt === 'number',
+          'updatedAt is a numeric timestamp'
+        );
+      },
+      changeStatus: () => {
+        assert.ok('true','button is clicked');
+      },
     });
     await render(hbs`
             <UserStatus 
                 @status={{this.status}} 
+                @changeStatus={{this.changeStatus}} 
                 @isStatusUpdating={{this.isStatusUpdating}}
+                @updateStatus={{this.updateStatus}} 
             />
             `);
 
@@ -28,6 +44,7 @@ module('Integration | Component | user-status', function (hooks) {
     assert
       .dom('[data-test-btn-label="ACTIVE"]')
       .hasText('Change your status to Active');
+    await click('[data-test-btn="ACTIVE"]');
 
     assert.dom('[data-test-btn="IDLE"]').exists();
     assert.dom('[data-test-btn="IDLE"]').hasClass('buttons__idle');
@@ -35,6 +52,7 @@ module('Integration | Component | user-status', function (hooks) {
     assert
       .dom('[data-test-btn-label="IDLE"]')
       .hasText('Change your status to Idle');
+    await click('[data-test-btn="IDLE"]');
 
     assert.dom('[data-test-btn="OOO"]').exists();
     assert.dom('[data-test-btn="OOO"]').hasClass('buttons__ooo');
@@ -42,17 +60,30 @@ module('Integration | Component | user-status', function (hooks) {
     assert
       .dom('[data-test-btn-label="OOO"]')
       .hasText('Change your status to OOO');
+    await click('[data-test-btn="OOO"]');
   });
 
   test('status is "ONBOARDING"', async function (assert) {
     this.setProperties({
       status: 'ONBOARDING',
       isStatusUpdating: false,
+      updateStatus: (statusPayLoad) => {
+        const {
+          currentStatus: { updatedAt, from, state  },
+        } = statusPayLoad;
+        assert.equal(state, 'ACTIVE', 'new state present in the payload');
+        assert.ok(typeof from === 'number', 'from is a numeric timestamp');
+        assert.ok(
+          typeof updatedAt === 'number',
+          'updatedAt is a numeric timestamp'
+        );
+      },
     });
     await render(hbs`
             <UserStatus 
                 @status={{this.status}} 
                 @isStatusUpdating={{this.isStatusUpdating}}
+                @updateStatus={{this.updateStatus}}
             />
             `);
 
@@ -75,16 +106,21 @@ module('Integration | Component | user-status', function (hooks) {
     assert
       .dom('[data-test-btn-label="ACTIVE"]')
       .hasText('Change your status to Active');
+    await click('[data-test-active-button]');
   });
 
   test('status is "ACTIVE"', async function (assert) {
     this.setProperties({
       status: 'ACTIVE',
       isStatusUpdating: false,
+      changeStatus: () => {
+        assert.ok('true','button is clicked');
+      },
     });
     await render(hbs`
             <UserStatus 
                 @status={{this.status}} 
+                @changeStatus={{this.changeStatus}} 
                 @isStatusUpdating={{this.isStatusUpdating}}
             />
             `);
@@ -98,6 +134,7 @@ module('Integration | Component | user-status', function (hooks) {
     assert
       .dom('[data-test-btn-label="IDLE"]')
       .hasText('Change your status to Idle');
+    await click('[data-test-btn="IDLE"]');
 
     assert.dom('[data-test-btn="OOO"]').exists();
     assert.dom('[data-test-btn="OOO"]').hasClass('buttons__ooo');
@@ -105,17 +142,34 @@ module('Integration | Component | user-status', function (hooks) {
     assert
       .dom('[data-test-btn-label="OOO"]')
       .hasText('Change your status to OOO');
+    await click('[data-test-btn="OOO"]');
   });
 
   test('status is "IDLE"', async function (assert) {
     this.setProperties({
       status: 'IDLE',
       isStatusUpdating: false,
+      updateStatus: (statusPayLoad) => {
+        const {
+          currentStatus: { updatedAt, from, state  },
+        } = statusPayLoad;
+        assert.equal(state, 'ACTIVE', 'new state present in the payload');
+        assert.ok(typeof from === 'number', 'from is a numeric timestamp');
+        assert.ok(
+          typeof updatedAt === 'number',
+          'updatedAt is a numeric timestamp'
+        );
+      },
+      changeStatus: () => {
+        assert.ok('true','button is clicked');
+      },
     });
     await render(hbs`
             <UserStatus 
                 @status={{this.status}} 
+                @changeStatus={{this.changeStatus}} 
                 @isStatusUpdating={{this.isStatusUpdating}}
+                @updateStatus={{this.updateStatus}}
             />
             `);
     assert.dom('[data-test-heading]').exists();
@@ -128,6 +182,7 @@ module('Integration | Component | user-status', function (hooks) {
     assert
       .dom('[data-test-btn-label="ACTIVE"]')
       .hasText('Change your status to Active');
+    await click('[data-test-btn="ACTIVE"]');
 
     assert.dom('[data-test-btn="OOO"]').exists();
     assert.dom('[data-test-btn="OOO"]').hasClass('buttons__ooo');
@@ -135,17 +190,34 @@ module('Integration | Component | user-status', function (hooks) {
     assert
       .dom('[data-test-btn-label="OOO"]')
       .hasText('Change your status to OOO');
+    await click('[data-test-btn="OOO"]');
   });
 
   test('status is "OOO"', async function (assert) {
     this.setProperties({
       status: 'OOO',
       isStatusUpdating: false,
+      updateStatus: (statusPayLoad) => {
+        const {
+          currentStatus: { updatedAt, from, state  },
+        } = statusPayLoad;
+        assert.equal(state, 'ACTIVE', 'new state present in the payload');
+        assert.ok(typeof from === 'number', 'from is a numeric timestamp');
+        assert.ok(
+          typeof updatedAt === 'number',
+          'updatedAt is a numeric timestamp'
+        );
+      },
+      changeStatus: () => {
+        assert.ok('true','button is clicked');
+      },
     });
     await render(hbs`
             <UserStatus 
                 @status={{this.status}} 
+                @changeStatus={{this.changeStatus}}
                 @isStatusUpdating={{this.isStatusUpdating}}
+                @updateStatus={{this.updateStatus}}
             />
             `);
 
@@ -159,6 +231,7 @@ module('Integration | Component | user-status', function (hooks) {
     assert
       .dom('[data-test-btn-label="ACTIVE"]')
       .hasText('Change your status to Active');
+    await click('[data-test-btn="ACTIVE"]');
 
     assert.dom('[data-test-btn="IDLE"]').exists();
     assert.dom('[data-test-btn="IDLE"]').hasClass('buttons__idle');
@@ -166,5 +239,6 @@ module('Integration | Component | user-status', function (hooks) {
     assert
       .dom('[data-test-btn-label="IDLE"]')
       .hasText('Change your status to Idle');
+    await click('[data-test-btn="IDLE"]');
   });
 });
