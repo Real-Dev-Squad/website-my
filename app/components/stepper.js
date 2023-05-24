f2b12e3ffa32c74ab1dc68771ada9909e83cb7e9
shubham bajaj <shmbajaj32@gmail.com>
2023-05-25 00:08:15 +0530

    refactor: styles and parsing props

--- app/components/stepper.js
+++ app/components/stepper.js
@@ -3,15 +3,15 @@ import Component from '@glimmer/component';
 export default class StepperComponent extends Component {
   get arePropsValid() {
     const { totalSteps, completedSteps } = this.args;
-    return (
-      Number.isInteger(totalSteps) &&
-      Number.isInteger(completedSteps) &&
-      totalSteps > 0
-    );
+    return !isNaN(totalSteps) && !isNaN(completedSteps) && totalSteps > 0;
   }
 
   get numberOfSteps() {
     const times = Number.parseInt(this.args.totalSteps, 10);
     return new Array(times);
+  }
+
+  get parsedCompletedSteps() {
+    return parseInt(this.args.completedSteps, 10);
   }
 }
