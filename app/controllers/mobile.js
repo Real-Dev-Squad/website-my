1df2e4c87040bb1214703ce3998f719a85d004b2
shreya-mishra <shreyamishra040398@gmail.com>
2023-06-03 22:14:25 +0530

    test done and design fix

--- app/controllers/mobile.js
+++ app/controllers/mobile.js
@@ -3,7 +3,11 @@ import { action } from '@ember/object';
 
 export default class MobileController extends Controller {
   @action buttonClicked() {
-    if (confirm('Are you sure you are the one who scanned this QR code?')) {
+    if (
+      confirm(
+        'Are you sure you are the one who scanned this QR code?  Do you want to proceed?'
+      )
+    ) {
       // will do api call to authenticate the user
     } else {
       // will cancel the login
--- tests/integration/components/qrcode-test.js
+++ tests/integration/components/qrcode-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { render } from '@ember/test-helpers';
+import { render, click, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { setupRenderingTest } from 'ember-qunit';
 
@@ -19,5 +19,35 @@ module('Integration | Component | qr-code', function (hooks) {
      <Qrcode />
     `);
     assert.dom('[data-test="qr-code"]').doesNotExist();
+  });
+
+  test('buttonClicked confirms and performs API call when confirmed', async function (assert) {
+    await render(hbs`
+      <MobileController />
+    `);
+    window.confirm = () => true;
+
+    await click(find('.btn'));
+
+    assert.ok(
+      window.confirm.calledWith(
+        'Are you sure you are the one who scanned this QR code? Do you want to proceed?'
+      )
+    );
+  });
+
+  test('buttonClicked cancels login when not confirmed', async function (assert) {
+    await render(hbs`
+      <MobileController />
+    `);
+
+    window.confirm = () => false;
+
+    await click(find('.btn'));
+    assert.ok(
+      window.confirm.calledWith(
+        'Are you sure you are the one who scanned this QR code? Do you want to proceed?'
+      )
+    );
   });
 });
