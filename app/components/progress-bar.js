5765e0e7e9a21275c80bf32398d3578932e250d8
Aryex82 <98796547+Aryex82@users.noreply.github.com>
2023-07-17 14:29:34 +0000

    progress bar component
    update yarn lock file
    
    fix failing test
    
    fix failing test
    
    Revert "progress bar component"
    
    This reverts commit a6343df1d1bf36f7765b1c11fa276b346aab5dc3.

---
+++ app/components/progress-bar.js
@@ -0,0 +1,45 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { debounce } from 'lodash';
+export default class ProgressBarComponent extends Component {
+  @tracked isEditable = false;
+  @tracked value = this.args.value;
+  lastEditTime = null;
+
+  @action turnEditModeOn() {
+    this.isEditable = true;
+    this.lastEditTime = Date.now();
+    this.setEditableToFalse();
+  }
+
+  setEditableToFalse() {
+    setTimeout(() => {
+      if (this.isEditable && Date.now() - this.lastEditTime >= 5000) {
+        this.isEditable = false;
+      } else {
+        this.setEditableToFalse();
+      }
+    }, 5000);
+  }
+
+  @action onInput(e) {
+    this.lastEditTime = Date.now();
+    this.value = e.target.value;
+    if (this.args.onInput) {
+      this.args.onInput(this.value);
+    }
+  }
+
+  @action onChange(e) {
+    this.lastEditTime = Date.now();
+    if (this.args.onChange) {
+      this.debouncedChange(e);
+    }
+  }
+
+  debouncedChange = debounce(async (e) => {
+    await this.args.onChange(e);
+    this.isEditable = false;
+  }, 1000);
+}
