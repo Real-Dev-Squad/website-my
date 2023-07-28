975ccad053eca8d31f26e6977381ec10768de0db
Aryex82 <98796547+Aryex82@users.noreply.github.com>
2023-07-28 18:19:00 +0530

    Progress bar UX Improvements (#449)
    adds a new progress bar for tasks card

---
+++ tests/integration/components/progress-bar-test.js
@@ -0,0 +1,86 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { find, render, fillIn, click, waitFor } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | progress-bar', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    await render(hbs`<ProgressBar />`);
+
+    assert.dom('[data-test-progress-bar]').exists();
+  });
+
+  test('it has edit button', async function (assert) {
+    await render(hbs`<ProgressBar />`);
+
+    assert.dom('[data-test-progress-bar-button]').exists();
+  });
+
+  test('it has clickable edit button', async function (assert) {
+    this.setProperties({
+      percentageCompleted: '10',
+      onUpdate: (value) => {
+        this.percentageCompleted = value;
+      },
+    });
+    await render(
+      hbs`<ProgressBar @value={{this.percentageCompleted}} @onUpdate={{this.onUpdate}} />`
+    );
+    const editButton = find('[data-test-progress-bar-button]');
+
+    await click(editButton);
+    assert.dom(editButton).containsText('10');
+  });
+
+  test('it should display the correct value on update', async function (assert) {
+    this.setProperties({
+      percentageCompleted: '10',
+      onUpdate: (value) => {
+        this.percentageCompleted = value;
+      },
+    });
+    await render(
+      hbs`<ProgressBar @value={{this.percentageCompleted}} @onUpdate={{this.onUpdate}} />`
+    );
+
+    const editButton = find('[data-test-progress-bar-button]');
+
+    await click(editButton);
+
+    const progressBarInput = find('[data-test-progress-bar]');
+
+    await fillIn(progressBarInput, '50');
+
+    assert.dom('[data-test-progress-bar-button]').exists();
+    assert.equal(progressBarInput.value, '50', "The value should be '50'.");
+  });
+
+  test('it should display the old value when an update fails', async function (assert) {
+    this.setProperties({
+      percentageCompleted: '10',
+      onChange: () => {
+        this.percentageCompleted = '10';
+      },
+    });
+    await render(
+      hbs`<ProgressBar @value={{this.percentageCompleted}} @onChange={{this.onChange}} />`
+    );
+
+    const editButton = find('[data-test-progress-bar-button]');
+
+    await click(editButton);
+    let progressBarInput = find('[data-test-progress-bar]');
+
+    await fillIn(progressBarInput, '50');
+
+    assert.dom('[data-test-progress-bar-button]').exists();
+
+    await waitFor(editButton);
+
+    await click(editButton);
+
+    assert.dom('[data-test-progress-bar-button]').hasText('10');
+  });
+});
