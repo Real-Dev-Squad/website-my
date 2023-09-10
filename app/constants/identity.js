f617d7f01fc5688e80cbc7ecd03e91bbcdca0bda
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2023-09-10 23:27:37 +0530

    Comments Resolved

---
+++ app/constants/identity.js
@@ -0,0 +1,14 @@
+export const ELEMENT_TYPE = {
+  CHECKBOX: 'CHECKBOX',
+  VERIFY_BUTTON: 'VERIFY_BUTTON',
+  SAVE_BUTTON: 'SAVE_BUTTON',
+};
+
+export const ERROR_TYPE = {
+  GENERATE_CHAINCODE: 'Generate Chaincode Not Clicked',
+  COPY_BUTTON: 'Copy Button Not Clicked',
+  CHECKBOX: 'Checkbox Not Clicked',
+  PROFILE_URL_NOT_FOUND: 'Please enter a Profile URL',
+  SAME_PROFILE_URL:
+    'If you want to save new URL, you have to modify Profile URL',
+};
