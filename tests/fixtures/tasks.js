0c6a62dcf00b7951761ad7e4201e6265187be12f
Joy <56365512+ardourApeX@users.noreply.github.com>
2023-12-19 23:44:00 +0530

    Bug fix : Red Task issue (#544)
    * Bug : Fixed task red issue on my site
    
    * Test : Added test for status conversion based on flag

--- tests/fixtures/tasks.js
+++ tests/fixtures/tasks.js
@@ -51,6 +51,32 @@ const tasks = [
     featureUrl: 'aliqua non minim',
     purpose: 'quis nostrud deserunt',
   },
+  {
+    id: 't5',
+    title: 'Task Object 1',
+    startedOn: 1662800000,
+    isNoteworthy: true,
+    endsOn: 1662803133,
+    percentCompleted: 100,
+    type: 'eu',
+    status: 'COMPLETED',
+    assignee: 'user1',
+    featureUrl: 'aliqua non minim',
+    purpose: 'quis nostrud deserunt',
+  },
+  {
+    id: 't6',
+    title: 'Task Object 1',
+    startedOn: 1662800000,
+    isNoteworthy: true,
+    endsOn: 1662803133,
+    percentCompleted: 100,
+    type: 'eu',
+    status: 'DONE',
+    assignee: 'user1',
+    featureUrl: 'aliqua non minim',
+    purpose: 'quis nostrud deserunt',
+  },
 ];
 
 const overDueTask = {
