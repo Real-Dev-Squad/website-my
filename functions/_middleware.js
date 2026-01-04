cd3cd3930e435cb2802438e3719d5a135e6cb07c
Mayank Bansal <mayankbansal125@gmail.com>
2026-01-05 01:44:56 +0530

    chore: add function for deprecation

---
+++ functions/_middleware.js
@@ -0,0 +1,20 @@
+export async function onRequest(context) {
+  const { env, next } = context;
+
+  const IS_DEPRECATED = env.DEPRECATED !== 'false';
+
+  if (IS_DEPRECATED) {
+    return new Response(
+      'This site has been deprecated. Please use main-site.',
+      {
+        status: 410,
+        headers: {
+          'Content-Type': 'text/plain; charset=utf-8',
+          'Cache-Control': 'no-store',
+        },
+      }
+    );
+  }
+
+  return next();
+}
